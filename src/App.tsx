f7ca9d80bfd6053788578897489167ac43518fe3
정형진 <pnescene@icloud.com>
2022-04-03 23:06:03 +0900

    스타일링 및 로직

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import React, { useState } from "react";
 import styled from "styled-components";
+import Timer from "./timer";
+
+const soundfile1 = require("./assets/click1.mp3");
+const soundfile2 = require("./assets/click2.mp3");
+const click1 = new Audio(soundfile1);
+const click2 = new Audio(soundfile2);
 
 const Container = styled.main`
   display: flex;
@@ -22,15 +29,9 @@ const BPMDisplay = styled.div`
     font-size: 4em;
   }
 `;
-const TempoText = styled.div`
-  font-size: 0.8em;
-  text-transform: uppercase;
-  text-align: center;
-`;
 const TempoSettings = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
   .adjust-tempo-btn {
     display: flex;
     justify-content: center;
@@ -38,9 +39,8 @@ const TempoSettings = styled.div`
     width: 30px;
     height: 30px;
     font-size: 20px;
-    border-radius: 50%;
+    border-radius: 15px;
     border: 1px solid #ddd;
-    text-align: center;
     cursor: pointer;
     &:hover {
       background: #fa545c;
@@ -122,39 +122,117 @@ const Measures = styled.div`
     line-height: 20px;
   }
 `;
-const BPMText = styled.span`
+const MeasureText = styled.span`
   text-align: center;
   font-size: 0.5em;
   text-transform: uppercase;
 `;
 
 function App() {
+  const [bpm, setBpm] = useState(140);
+  const [beatsPerMeasure, setBeatsPerMeasure] = useState(4);
+  const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const onDecreaseClick = () => {
+    if (bpm <= 20) return;
+    setBpm((prev) => prev + 1);
+    updateMetronome();
+  };
+  const onIncreaseClick = () => {
+    if (bpm >= 280) return;
+    setBpm((prev) => prev + 1);
+    updateMetronome();
+  };
+  const onSliderChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setBpm(Number(event.currentTarget.value));
+    updateMetronome();
+  };
+  const onBeatsSubtract = () => {
+    if (beatsPerMeasure <= 2) return;
+    setBeatsPerMeasure((prev) => prev - 1);
+    setCount(0);
+  };
+  const onBeatsAdd = () => {
+    if (beatsPerMeasure >= 12) return;
+    setBeatsPerMeasure((prev) => prev + 1);
+    setCount(0);
+  };
+  const onStartStopClick = () => {
+    setCount(0);
+    if (!isRunning) {
+      metronome.start();
+      setIsRunning(true);
+    } else {
+      metronome.stop();
+      setIsRunning(false);
+    }
+  };
+
+  const updateMetronome = () => {
+    metronome.timeInterval = 60000 / bpm;
+  };
+
+  const playClick = () => {
+    console.log(count);
+    if (count === beatsPerMeasure) {
+      setCount(0);
+    }
+    if (count === 0) {
+      click1.play();
+      click1.currentTime = 0;
+    } else {
+      click2.play();
+      click2.currentTime = 0;
+    }
+    setCount((prev) => prev + 1);
+  };
+
+  const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });
+
   return (
     <Container>
       <Metronome>
         <BPMDisplay>
-          <span className="tempo">140</span>
+          <span className="tempo">{bpm}</span>
           <span className="bpm">BPM</span>
         </BPMDisplay>
-        <TempoText>Nice and steady</TempoText>
         <TempoSettings>
-          <div className="adjust-tempo-btn decrease-tempo">-</div>
+          <div
+            className="adjust-tempo-btn decrease-tempo"
+            onClick={onDecreaseClick}
+          >
+            -
+          </div>
           <BPMSlider
             type="range"
             min="20"
             max="280"
             step="1"
+            value={bpm}
             className="slider"
+            onChange={onSliderChange}
           />
-          <div className="adjust-tempo-btn increase-tempo">+</div>
+          <div
+            className="adjust-tempo-btn increase-tempo"
+            onClick={onIncreaseClick}
+          >
+            +
+          </div>
         </TempoSettings>
-        <StartStop>START</StartStop>
+        <StartStop onClick={onStartStopClick}>
+          {isRunning ? "STOP" : "START"}
+        </StartStop>
         <Measures>
-          <div className="subtract-beats stepper">-</div>
-          <div className="measure-count">4</div>
-          <div className="add-beats stepper">+</div>
+          <div className="subtract-beats stepper" onClick={onBeatsSubtract}>
+            -
+          </div>
+          <div className="measure-count">{beatsPerMeasure}</div>
+          <div className="add-beats stepper" onClick={onBeatsAdd}>
+            +
+          </div>
         </Measures>
-        <BPMText>Beats per measure</BPMText>
+        <MeasureText>Beats per measure</MeasureText>
       </Metronome>
     </Container>
   );
